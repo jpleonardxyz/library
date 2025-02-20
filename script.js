a47d371fdddfc6b0e29f48f4a64a932308d7cd2a
jpleonardxyz <169338128+jpleonardxyz@users.noreply.github.com>
2025-02-20 09:29:20 +0200

    Add constructor and helper for Book object

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(title, author, pages, isread) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isread = isread;
+}
+
+function addBookToLibrary(title, author, pages, isread) {
+    const newBook = new Book(title, author, pages, isread);
+    myLibrary.push(newBook);
+}
