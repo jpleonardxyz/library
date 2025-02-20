308f887fee5ba6c201255a3fbb64b777ea27a405
jpleonardxyz <169338128+jpleonardxyz@users.noreply.github.com>
2025-02-20 10:14:00 +0200

    Add function to display all books in library

--- script.js
+++ script.js
@@ -11,3 +11,37 @@ function addBookToLibrary(title, author, pages, isread) {
     const newBook = new Book(title, author, pages, isread);
     myLibrary.push(newBook);
 }
+
+function displayBooks() {
+    const container = document.querySelector(".container");
+    
+    for (const curBook of myLibrary) {
+        console.log(curBook);
+        const card = createNewCard(curBook);
+        container.appendChild(card);
+    }
+}
+
+function createNewCard(curBook) {
+    const newCard = document.createElement("div");
+
+    const title = document.createElement("h2");
+    title.textContent = curBook.title;
+
+    const author = document.createElement("h3");
+    author.textContent = "by " + curBook.author;
+
+    const pages = document.createElement("p");
+    pages.textContent = curBook.pages + " pages";
+
+    const read = document.createElement("p");
+    curBook.isread ? read.textContent = "Is read" : read.textContent = "Not read";
+
+    newCard.appendChild(title);
+    newCard.appendChild(author);
+    newCard.appendChild(pages);
+    newCard.appendChild(read);
+
+    newCard.classList.add("card");
+    return newCard;
+}
